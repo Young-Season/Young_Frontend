23cf9a4fc46d10d4154cb3f17b4cbad0894d2891
jwooklee <jaewook04@gmail.com>
2023-11-21 01:59:50 +0900

    design: 하얀색 설명 박스 디자인 완료

---
+++ src/components/layout/WhiteBox.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const WhiteBox = () => {
+  return (
+    <WhiteContainer>
+      <div>ㅇㅇ</div>
+    </WhiteContainer>
+  );
+};
+
+export default WhiteBox;
+
+const WhiteContainer = styled.div`
+  display: flex;
+  width: 20rem;
+  padding: 1.25rem 1rem;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 1.25rem;
+  border-radius: 0.75rem;
+  background: #fff;
+`;
