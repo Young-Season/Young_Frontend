61971b9a9ced1b8657ec2ec71ad3353ec39e29bc
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-11-21 20:53:36 +0900

    design: StartButton 컴포넌트 디자인 완료

---
+++ src/components/layout/StartButton.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const StartButton = () => {
+  return (
+    <StartButtonContainer>시작하기</StartButtonContainer>
+  )
+}
+
+export default StartButton;
+
+const StartButtonContainer = styled.div`
+display: flex;
+width: 120px;
+height: 36px;
+justify-content: center;
+align-items: center;
+gap: 10px;
+
+border-radius: 16px;
+background: var(--Brown, #64422E);
+
+color: var(--White, #FAFAFA);
+text-align: center;
+font-family: Spoqa Han Sans Neo;
+font-size: 14px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`
