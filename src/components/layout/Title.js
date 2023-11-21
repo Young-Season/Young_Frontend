13145f5da24ac848b435ad6636692cfb9f5561c4
kiiiiv <lizzy2@naver.com>
2023-11-21 17:13:07 +0900

    [FEAT] Description, Title칸 만들었음.

---
+++ src/components/layout/Title.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Title(props) {
+
+  return (
+    <Text>{props}</Text>
+  );
+}
+
+export default Title;
+
+const Wrapper = styled.div`
+
+width: 222px;
+height: 25px;
+
+font-family: 'Spoqa Han Sans Neo';
+font-style: normal;
+font-weight: 700;
+font-size: 20px;
+line-height: 25px;
+
+color: #64422E;
+
+`;
+
