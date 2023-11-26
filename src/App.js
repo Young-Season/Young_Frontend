efb5b42e7d266937d12523a91c5e4a377a1b46ea
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-11-27 01:42:14 +0900

    feat: HostNicknamePage 구현 완료 및 dissolve효과에 필요한 react-transition-group설치

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HostLoginPage from "./pages/host/HostLoginPage";
+import GuestFacePage from "./pages/guest/GuestFacePage";
+import GuestEmojiPage from "./pages/guest/GuestEmojiPage";
+import GuestColorPage from "./pages/guest/GuestColorPage";
+import GuestFirstImpressionPage from "./pages/guest/GuestFirstImpressionPage";
+import GuestPresentImpresssionPage from "./pages/guest/GuestPresentImpresssionPage";
+import HostUrlDeployPage from "./pages/host/HostUrlDeployPage";
+import { RecoilRoot } from 'recoil'; // RecoilRoot 추가\
+import HostLoadingPage from "./pages/host/HostLoadingPage";
+import HostNicknamePage from "./pages/host/HostNicknamePage";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HostLoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    <RecoilRoot> {/* RecoilRoot로 앱 감싸기 */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HostLoginPage />} />
+          <Route path="/guestface" element={<GuestFacePage />} />
+          <Route path="/guestemoji" element={<GuestEmojiPage />} />
+          <Route path="/guestcolor" element={<GuestColorPage />} />
+          <Route path="/firstimpression" element={<GuestFirstImpressionPage />} />
+          <Route path="/presentimpression" element={<GuestPresentImpresssionPage />} />
+          <Route path="/deploy" element={<HostUrlDeployPage />} />
+          <Route path="/hostLoading" element={<HostLoadingPage/>}/>
+          <Route path="/hostNickname" element={<HostNicknamePage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 };
 
--- src/pages/host/HostLoadingPage.js
+++ src/pages/host/HostLoadingPage.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import styled from 'styled-components';
+import BigButton from '../../../src/components/layout/BigButton';
 
 const HostLoadingPage = () => {
+  const imageUrl = process.env.PUBLIC_URL + '/images/BG_blur.png';
+
+
+  const startKakao = () => {
+    //카카오 로그인 여기에 구현
+    console.log('start');
+  }
+
   return (
-    <div>HostLoadingPage</div>
+    <BackGround>
+
+      <Image bg={imageUrl}>
+        <Contents>
+          <Text>내 공간 생성 완료!</Text>
+        </Contents>      
+      </Image>
+    </BackGround>
   )
 }
 
-export default HostLoadingPage
+export default HostLoadingPage;
+
+const BackGround = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const Image = styled.div`
+  background-image: url(${props => props.bg});
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-size: cover;
+  background-position: center;
+  width: 375px;
+  height: 812px;
+  flex-shrink: 0;
+`
+
+const Contents = styled.div`
+flex-direction: column;
+align-items: center;
+
+width: 280px;
+display: flex;
+`
+const Text = styled.div`
+color: var(--Brown, #64422E);
+font-family: Spoqa Han Sans Neo;
+font-size: 20px;
+font-style: normal;
+font-weight: 700;
+line-height: normal;
+`
