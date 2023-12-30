a8d527529ad38fb3227d6ffc1b5d39512cb5e6a7
Youngeun Jun <121297085+Junyewdd@users.noreply.github.com>
2023-12-31 00:57:52 +0900

    refactor: atom.js 충돌 해결

--- src/apis/login.js
+++ src/apis/login.js
@@ -15,27 +15,10 @@ export const getLogin = async () => {
   }
 }
 
-// export const postkakaoCallback = async (code) => {
-//   try{
-//     const url = `${baseURL}/oauth/kakao/callback`;
-//     axios.post('/oauth/kakao/callback', { code })
-//     .then((response) => {
-//       console.log(response.data);  // 서버로부터 받은 응답을 출력
-//       return response.data;
-//     })
-//     .catch((error) => {
-//       console.error(error);  // 오류를 출력
-//     });
-//   }
-//   catch(error){
-//     console.error(error);
-//   }
-// }
-
 export const postkakaoCallback = async (code) => {
   try{
     const url = `${baseURL}/oauth/kakao/callback`;
-    axios.get('/oauth/kakao/callback')
+    axios.get('/oauth/kakao/callback', { code })
     .then((response) => {
       console.log(response.data);  // 서버로부터 받은 응답을 출력
       return response.data;
@@ -48,3 +31,20 @@ export const postkakaoCallback = async (code) => {
     console.error(error);
   }
 }
+
+// export const postkakaoCallback = async (code) => {
+//   try{
+//     const url = `${baseURL}/oauth/kakao/callback`;
+//     axios.get('/oauth/kakao/callback')
+//     .then((response) => {
+//       console.log(response.data);  // 서버로부터 받은 응답을 출력
+//       return response.data;
+//     })
+//     .catch((error) => {
+//       console.error(error);  // 오류를 출력
+//     });
+//   }
+//   catch(error){
+//     console.error(error);
+//   }
+// }
