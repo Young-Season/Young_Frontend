010e81e4497c6687c9adcfc68046b6f7cd0037b6
kiiiiv <lizzy2@naver.com>
2023-12-31 00:55:15 +0900

    [Refactor]충돌해결

--- src/atom.js
+++ src/atom.js
@@ -1,19 +1,15 @@
 import { atom } from 'recoil';
 export const animalImageState = atom({
     key: "animalImageState",
-<<<<<<< HEAD
-    default: "",
+    default: "https://young-season.o-r.kr/public/images/000.png",
 });
 
 export const userIdState = atom({
     key: 'userIdState',
     default: null,
 });
-=======
-    default: "https://young-season.o-r.kr/public/images/000.png",
-})
+
 export const arrayState = atom({
     key: "arrayState",
     default: ["0", "0", "0", "0", "0"],
 })
->>>>>>> acd3e1d0a439f81cdb81f1c106b5e32dc006c574
