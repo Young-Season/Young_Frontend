d0cc6c999f6ee2763913883c89b9194be0220f4a
jwooklee <jaewook04@gmail.com>
2024-01-01 15:47:32 +0900

    fix: 호스트 api 수정

--- src/apis/host.js
+++ src/apis/host.js
@@ -1,9 +1,47 @@
 import axios from "axios";
 import { atom, useRecoilState, useRecoilValue } from "recoil";
+import { tokenState } from "../atom";
 
 export const baseUrl = "https://young-season.o-r.kr";
 
-export const getHostTotalResult = (hostId) => {
-  const url = `${baseUrl}/data/${hostId}`;
-  return axios.get(url);
+export const getHostTotalResult = async (token, hostId) => {
+  const url = `${baseUrl}/data/:${hostId}`;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getHostIndividualResult = async (token, hostId, guestId) => {
+  const url = `${baseUrl}/data/:${hostId}/:${guestId}`;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getHostStats = async (token, hostId) => {
+  const url = `${baseUrl}/stats/:${hostId}`;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
 };
