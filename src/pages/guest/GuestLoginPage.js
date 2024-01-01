4045da3f308594cf9de1e8e97578624a4eb02cc3
kiiiiv <lizzy2@naver.com>
2024-01-02 01:53:46 +0900

    [Feat]로그인 페이지

--- src/pages/guest/GuestLoginPage.js
+++ src/pages/guest/GuestLoginPage.js
@@ -63,6 +63,7 @@ const GuestLoginPage = () => {
   }
   useEffect(()=>{
     const fetchData = async () =>{
+      console.log(hostId);
       const urlParams = new URLSearchParams(location.search);
       const hostId = urlParams.get('hostId');
       setUserId(hostId);
@@ -88,7 +89,7 @@ const GuestLoginPage = () => {
       <Image bg={imageUrl}>
       
         <Contents>
-          <Text>내가 생각하는</Text>
+          <Text>내가 생각하는{hostId}</Text>
           <Text>{set_prepositional_particle(hostNickname)}?</Text>
           <NicknameBox>
 
