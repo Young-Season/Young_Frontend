b6f90363dbff7b00e6193c2ff201e94563e8549a
jwooklee <jaewook04@gmail.com>
2024-01-03 07:14:31 +0900

    fix: history 라이브러리 오류 나서 삭제

--- src/pages/guest/GuestFacePage.js
+++ src/pages/guest/GuestFacePage.js
@@ -4,13 +4,10 @@ import SmallButton from "../../components/layout/SmallButton";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
-// import { createBrowserHistory } from "history";
 
 import { hostNicknameState, usePostResponses } from "../../apis/guest";
 import { useRecoilState } from "recoil";
 import { animalImageState, arrayState } from "../../atom";
-
-// const history = createBrowserHistory();
 
 function GuestFacePage() {
   const hostName = useRecoilValue(hostNicknameState);
@@ -40,7 +37,7 @@ function GuestFacePage() {
 
       resolve();
     });
-    navigate("/guestemoji");
+    navigate("/guestemoji", { replace: true });
   };
 
   const getSubjectSuffix = (name) => {
@@ -51,19 +48,6 @@ function GuestFacePage() {
     }
     return (lastCharCode - 44032) % 28 === 0 ? "는" : "은";
   };
-
-  // useEffect(() => {
-  //   // const unblock = history.block((location, action) => {
-  //   //   if (action === "POP") {
-  //   //     // 뒤로가기 동작 방지
-  //   //     return false;
-  //   //   }
-  //   // });
-
-  //   // return () => {
-  //   //   unblock(); // 컴포넌트가 언마운트되었을 때 블록 해제
-  //   // };
-  // }, []);
 
   return (
     <FaceContainer>
